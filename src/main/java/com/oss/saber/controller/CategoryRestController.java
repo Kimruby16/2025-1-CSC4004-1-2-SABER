56b7c9ab18de9ad1fc7af57e5b6a63ac7936eb39
jieun13 <ynb9598@gmail.com>
2025-05-14 23:34:43 +0900

    feat: swagger 추가

--- src/main/java/com/oss/saber/controller/CategoryRestController.java
+++ src/main/java/com/oss/saber/controller/CategoryRestController.java
@@ -6,6 +6,8 @@ import com.oss.saber.dto.DefaultVerificationResponse;
 import com.oss.saber.dto.mapper.CategoryMapper;
 import com.oss.saber.dto.mapper.DefaultVerificationMapper;
 import com.oss.saber.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +18,13 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "품목 카테고리 API", description = "카테고리 조회 및 카테고리 별 기본 인증 방식 추천 내용 조회")
 public class CategoryRestController {
 
     private final CategoryService categoryService;
 
     @GetMapping("/categories")
+    @Operation(summary = "품목 카테고리 조회", description = "물품 카테고리를 모두 불러옵니다.")
     public ResponseEntity<List<CategoryResponse>> getCategories() {
         List<Category> categories = categoryService.getAllCategories();
         List<CategoryResponse> responses = categories.stream()
@@ -31,6 +35,7 @@ public class CategoryRestController {
     }
 
     @GetMapping("/categories/{categoryId}")
+    @Operation(summary = "기본 인증 방식 조회", description = "해당 품목의 기본 인증 방식을 조회할 수 있습니다. ")
     public ResponseEntity<List<DefaultVerificationResponse>> getDefaultVerifications(@PathVariable Long categoryId) {
         Category category = categoryService.getCategoryById(categoryId);
         List<DefaultVerificationResponse> responses = category.getDefaultVerifications().stream()
--- src/main/java/com/oss/saber/controller/VerificationLinkRestController.java
+++ src/main/java/com/oss/saber/controller/VerificationLinkRestController.java
@@ -7,6 +7,8 @@ import com.oss.saber.dto.VerificationLinkSettingRequest;
 import com.oss.saber.dto.VerificationLinkStatusResponse;
 import com.oss.saber.dto.mapper.VerificationLinkStatusMapper;
 import com.oss.saber.service.VerificationLinkService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "인증 링크 API", description = "")
 public class VerificationLinkRestController {
 
     @Value("${app.base-url}")
@@ -23,18 +26,21 @@ public class VerificationLinkRestController {
     private final VerificationLinkService verificationLinkService;
 
     @PostMapping("/verification/settings/category")
+    @Operation(summary = "카테고리 선택", description = "품목의 카테고리를 선택합니다.")
     public ResponseEntity<String> selectCategory(@RequestBody CategorySettingRequest request) {
         verificationLinkService.createLink(request.getCategoryId());
         return ResponseEntity.ok("카테고리 설정 완료");
     }
 
     @PostMapping("/verification/{verificationId}/settings")
+    @Operation(summary = "상세 인증 방식 선택", description = "원하는 인증 방식을 설정합니다. 시간, 방법 등")
     public ResponseEntity<String> setVerificationOptions(@PathVariable Long verificationId, @RequestBody VerificationLinkSettingRequest request) {
         verificationLinkService.settingVerificationLink(verificationId, request);
         return ResponseEntity.ok("상세 설정 완료");
     }
 
     @PostMapping("/verification/{verificationId}/link")
+    @Operation(summary = "인증 링크 생성", description = "인증이 진행될 링크를 생성합니다.")
     public ResponseEntity<VerificationLinkResponse> createVerificationLink(@PathVariable Long verificationId) {
         VerificationLink link = verificationLinkService.getVerificationLink(verificationId);
 
@@ -47,6 +53,7 @@ public class VerificationLinkRestController {
     }
 
     @GetMapping("/verification/{verificationId}")
+    @Operation(summary = "인증 진행 상태 조회", description = "인증 진행 상태를 조회합니다.")
     public ResponseEntity<VerificationLinkStatusResponse> getVerificationStatus(@PathVariable Long verificationId) {
         VerificationLink verificationLink = verificationLinkService.getVerificationLink(verificationId);
         VerificationLinkStatusResponse response = VerificationLinkStatusMapper.toResponse(verificationLink);
